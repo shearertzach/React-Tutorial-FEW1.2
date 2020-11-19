b620d48aa395db5facd003e4f28aae29ffc89717
shearertzach <shearertzach@gmail.com>
2020-11-18 19:08:19 -0600

    Created a json file, imported the data, and parsed it out

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import './Footer.css'
 
+function getYear() {
+  const today = new Date()
+  return today.getFullYear()
+}
+
 function Footer() {
   return (
     <div className="Footer">
-      <h3>Zach Shearer copyright 2020</h3>
+      <h3>Zach Shearer copyright {getYear()}</h3>
     </div>
   )
 }
